bd57b608a3cfd3c1b57db12f2c83ef9d16ded473
dumindupd <Quick@179>
2021-05-24 17:27:46 +0530

    Second Commit

---
+++ app/src/main/java/com/example/myshoppingapp/OrderHistoryFragment.java
@@ -0,0 +1,24 @@
+package com.example.myshoppingapp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+public class OrderHistoryFragment extends Fragment {
+
+    public OrderHistoryFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view =  inflater.inflate(R.layout.fragment_order_history, container, false);
+
+        return view;
+    }
+}
